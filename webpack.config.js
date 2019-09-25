f7e33dccc783aa1df29bd1c6464dea2decdfc675
JP Given <jgiven@gmail.com>
2019-09-25 15:45:02 -0700

    Things are working in IE now. On to some real development.

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     rules: [
       {
         test: /.js$/,
-        exclude: /node_modules/,
+        // exclude: /node_modules/,
         include: [
           /node_modules(?:\/|\\)lit-element|lit-html/
         ],
