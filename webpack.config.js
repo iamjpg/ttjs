04f8377a9194622d06dbf10e9b41d4bebc010fc4
JP Given <jgiven@gmail.com>
2019-09-25 13:26:33 -0700

    Initial webpack set up completed - now the real work begins.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,26 @@
+var path = require('path');
+
 module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'platypus.bundle.js'
-  }
+  },
+  module: {
+  rules: [
+    {
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      include: [
+          /node_modules(?:\/|\\)lit-element|lit-html/
+        ],
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    }
+  ]
+}
 };
