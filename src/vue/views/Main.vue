73fb552067b975f90e0613432be094be6b60d9ae
JP Given <jgiven@gmail.com>
2019-09-26 15:23:54 -0700

    Initial messaging.

--- src/vue/views/Main.vue
+++ src/vue/views/Main.vue
@@ -3,15 +3,15 @@
     <div id="tagline">
       <h1>Turntable is a component and CSS library providing framework agnostic components, written in <a href="https://lit-element.polymer-project.org/" target="_blank">lit-element</a>, to be used in all popular JavaScript Frameworks.</h1>
       <p>
-        As it says above, the goal of this component library is to be framework agnostic. We chose TailwindCSS as the foundation and Lit-Element as the component integration. 
-        We chose these frameworks to build on because functional CSS is awesome and lit-element allows flexibility in the implementation of components. These components 
+        As it says above, the goal of this component library is to be framework agnostic. We chose <a href="https://tailwindcss.com" target="_blank">TailwindCSS</a> as the foundation and <a href="https://lit-element.polymer-project.org/" target="_blank">Lit-Element</a> as the component integration. 
+        We chose these frameworks to build on because functional CSS is awesome and Lit-Element allows flexibility in the implementation of components. These components 
         can even be used within the normal DOM thanks to the WebComponents.org polyfill.
       </p>
+      <div id="tt-loves-vue">
+        <i class="far fa-turntable"></i> + <i class="fab fa-vuejs" style="color: green;"></i> = <i class="fas fa-heart" style="color: red;"></i>
+      </div>
       <p>
-        <img src="../assets/vue-logo.png" width="200" id="vue-logo">
-      </p>
-      <p>
-        In an attempt to "eat our own dog food" the site you're currently viewing is a VueJS application loading Turntable components written in lit-element. 
+        In an attempt to "eat our own dog food" the site you're currently viewing is a <a href="https://vuejs.org" target="_blank">VueJS</a> application loading Turntable components written in lit-element. 
         For more information on framework integration, feel free to jump ahead.
       </p>
     </div>
@@ -38,8 +38,9 @@
       font-size: 40px;
     }
 
-    #vue-logo {
-      margin: auto;
+    #tt-loves-vue {
+      text-align: center;
+      font-size: 52px;
     }
   }
 </style>
