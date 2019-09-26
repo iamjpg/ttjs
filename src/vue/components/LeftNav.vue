32163d1e2ac57045902cc053b5824c5bc50297da
JP Given <jgiven@gmail.com>
2019-09-26 12:53:04 -0700

    Stubbed out the navigation.

--- src/vue/components/LeftNav.vue
+++ src/vue/components/LeftNav.vue
@@ -8,6 +8,16 @@
       <li>Getting Started</li>
       <li class="title">CSS</li>
       <li>Typography</li>
+      <li>Colors</li>
+      <li>Grid</li>
+      <li>Containers</li>
+      <li>Box Shadows</li>
+      <li>Badges</li>
+      <li class="title">Components</li>
+      <li>Buttons</li>
+      <li>Form Elements</li>
+      <li>Toasts</li>
+      <li>Cards</li>
     </ul>
   </section>
 </template>
@@ -32,6 +42,7 @@ export default {
     width: 250px;
     border-right: 3px solid #e6e6e6;
     background: #fff;
+    overflow-y: auto;
 
     #logo-container {
       border-bottom: 1px solid #e6e6e6;
@@ -52,9 +63,15 @@ export default {
       margin: 0;
       padding: 0;
 
-      li {
+      li:not(.title) {
         padding: 15px 25px;
         border-bottom: 1px solid #e6e6e6;
+      }
+
+      li.title {
+        font-weight: bold;
+        font-style: italic;
+        padding: 15px 25px 5px;
       }
 
     }
