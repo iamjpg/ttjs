bf34ccf3b3cd925de0a2017b434a53566aedb85b
JP Given <jgiven@gmail.com>
2019-10-01 10:08:11 -0700

    Updating CSS prefixes. Updated Grid Gists.

--- src/vue/views/Grid.vue
+++ src/vue/views/Grid.vue
@@ -7,38 +7,38 @@
     <h2>Static Grid</h2>
 
     <!-- Full width column -->
-    <div class="v-flex">
-      <div class="v-flex-item w-full bg-green text-center">1</div>
+    <div class="tt-flex">
+      <div class="tt-flex-item w-full bg-green text-center">1</div>
     </div>
 
     <!-- Two columns -->
-    <div class="v-flex">
-      <div class="v-flex-item w-1/2 bg-green text-center">1</div>
-      <div class="v-flex-item w-1/2 bg-green-light text-center">2</div>
+    <div class="tt-flex">
+      <div class="tt-flex-item w-1/2 bg-green text-center">1</div>
+      <div class="tt-flex-item w-1/2 bg-green-light text-center">2</div>
     </div>
 
     <!-- Three columns -->
-    <div class="v-flex">
-      <div class="v-flex-item w-1/3 bg-green text-center">1</div>
-      <div class="v-flex-item w-1/3 bg-green-light text-center">2</div>
-      <div class="v-flex-item w-1/3 bg-green text-center">3</div>
+    <div class="tt-flex">
+      <div class="tt-flex-item w-1/3 bg-green text-center">1</div>
+      <div class="tt-flex-item w-1/3 bg-green-light text-center">2</div>
+      <div class="tt-flex-item w-1/3 bg-green text-center">3</div>
     </div>
 
     <!-- Four columns -->
-    <div class="v-flex">
-      <div class="v-flex-item w-1/4 bg-green text-center">1</div>
-      <div class="v-flex-item w-1/4 bg-green-light text-center">2</div>
-      <div class="v-flex-item w-1/4 bg-green text-center">3</div>
-      <div class="v-flex-item w-1/4 bg-green-light text-center">4</div>
+    <div class="tt-flex">
+      <div class="tt-flex-item w-1/4 bg-green text-center">1</div>
+      <div class="tt-flex-item w-1/4 bg-green-light text-center">2</div>
+      <div class="tt-flex-item w-1/4 bg-green text-center">3</div>
+      <div class="tt-flex-item w-1/4 bg-green-light text-center">4</div>
     </div>
 
     <!-- Five columns -->
-    <div class="v-flex">
-      <div class="v-flex-item w-1/5 bg-green text-center">1</div>
-      <div class="v-flex-item w-1/5 bg-green-light text-center">2</div>
-      <div class="v-flex-item w-1/5 bg-green text-center">3</div>
-      <div class="v-flex-item w-1/5 bg-green-light text-center">4</div>
-      <div class="v-flex-item w-1/5 bg-green text-center">5</div>
+    <div class="tt-flex">
+      <div class="tt-flex-item w-1/5 bg-green text-center">1</div>
+      <div class="tt-flex-item w-1/5 bg-green-light text-center">2</div>
+      <div class="tt-flex-item w-1/5 bg-green text-center">3</div>
+      <div class="tt-flex-item w-1/5 bg-green-light text-center">4</div>
+      <div class="tt-flex-item w-1/5 bg-green text-center">5</div>
     </div>
 
     <Gist username="iamjpg" hash="5b328884d49de9c1ec0119eb6e669c8c" />
@@ -46,22 +46,22 @@
     <h2>Responsive Grid</h2>
 
     <!-- Six columns -->
-    <div class="v-flex-wrap">
-      <div class="v-flex-item w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 bg-green text-center">1</div>
-      <div class="v-flex-item w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 bg-green-light text-center">2</div>
-      <div class="v-flex-item w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 bg-green text-center">3</div>
-      <div class="v-flex-item w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 bg-green-light text-center">4</div>
-      <div class="v-flex-item w-full sm:w-1/2 md:w-1/3 lg:w-1/2 xl:w-1/6 bg-green text-center">5</div>
-      <div class="v-flex-item w-full sm:w-1/2 md:w-1/3 lg:w-1/2 xl:w-1/6 bg-green-light text-center">6</div>
+    <div class="tt-flex-wrap">
+      <div class="tt-flex-item w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 bg-green text-center">1</div>
+      <div class="tt-flex-item w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 bg-green-light text-center">2</div>
+      <div class="tt-flex-item w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 bg-green text-center">3</div>
+      <div class="tt-flex-item w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 bg-green-light text-center">4</div>
+      <div class="tt-flex-item w-full sm:w-1/2 md:w-1/3 lg:w-1/2 xl:w-1/6 bg-green text-center">5</div>
+      <div class="tt-flex-item w-full sm:w-1/2 md:w-1/3 lg:w-1/2 xl:w-1/6 bg-green-light text-center">6</div>
     </div>
 
-    <div class="v-flex-wrap">
-      <div class="v-flex-item w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 bg-green text-center">7</div>
-      <div class="v-flex-item w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 bg-green-light text-center">8</div>
-      <div class="v-flex-item w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 bg-green text-center">9</div>
-      <div class="v-flex-item w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 bg-green-light text-center">10</div>
-      <div class="v-flex-item w-full sm:w-1/2 md:w-1/3 lg:w-1/2 xl:w-1/6 bg-green text-center">11</div>
-      <div class="v-flex-item w-full sm:w-1/2 md:w-1/3 lg:w-1/2 xl:w-1/6 bg-green-light text-center">12</div>
+    <div class="tt-flex-wrap">
+      <div class="tt-flex-item w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 bg-green text-center">7</div>
+      <div class="tt-flex-item w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 bg-green-light text-center">8</div>
+      <div class="tt-flex-item w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 bg-green text-center">9</div>
+      <div class="tt-flex-item w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 bg-green-light text-center">10</div>
+      <div class="tt-flex-item w-full sm:w-1/2 md:w-1/3 lg:w-1/2 xl:w-1/6 bg-green text-center">11</div>
+      <div class="tt-flex-item w-full sm:w-1/2 md:w-1/3 lg:w-1/2 xl:w-1/6 bg-green-light text-center">12</div>
     </div>
 
     <Gist username="iamjpg" hash="709781a0145187e32ede146060fc502b" />
